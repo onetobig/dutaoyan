98c14d56082cd8d4185733ab11275406d0c051f5
onetobig <onetobig@gmail.com>
2018-11-14 14:15:41 +0800

    添加后台

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<div class="footer">
+	<div class="container">
+		<p class="text-center">
+			由 <a href="#" target="_blank">onetobig</a> 设计和编码
+			<span style="color: #e27575;font-size: 14px;">♬</span>
+		</p>
+	</div>
+</div>
+
