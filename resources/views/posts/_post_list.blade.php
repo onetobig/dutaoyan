c85314b70cd6dfe13730401c8ee3f8e71d371094
onetobig <onetobig@gmail.com>
2018-11-21 10:59:34 +0800

    文章链接

--- resources/views/posts/_post_list.blade.php
+++ resources/views/posts/_post_list.blade.php
@@ -1,41 +1,41 @@
 @if(count($posts))
 	<ul class="media-list">
-		@foreach($posts as $topic)
+		@foreach($posts as $post)
 			<li class="media">
 				<div class="media-left">
 					<a href="{{ route('posts.show', ['post' => $post] )}}">
-						<img src="{{ $topic->cover_url }}" alt="" class="media-object img-thumbnail" style="max-width: 252px;" title="{{ $topic->user->name }}">
+						<img src="{{ $post->cover_url }}" alt="" class="media-object img-thumbnail" style="max-width: 252px;" title="{{ $post->user->name }}">
 					</a>
 				</div>
 				
 				<div class="media-body">
 					<div class="media-heading">
-						<a href="{{ $topic->link() }}" title="{{ $topic->title }}">
-							{{ $topic->title }}
+						<a href="{{ $post->link() }}" title="{{ $post->title }}">
+							{{ $post->title }}
 						</a>
 						
-						<a href="{{ $topic->link() }}" class="pull-right">
-							<span class="badge">{{ $topic->reply_count }}</span>
+						<a href="{{ $post->link() }}" class="pull-right">
+							<span class="badge">{{ $post->reply_count }}</span>
 						</a>
 					</div>
 					
 					<div class="media-body meta">
-						{{--<a href="{{ route('categories.show', [$topic->category->id]) }}" title="{{ $topic->category->name }}">--}}
+						{{--<a href="{{ route('categories.show', [$post->category->id]) }}" title="{{ $post->category->name }}">--}}
 							{{--<span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>--}}
-							{{--{{ $topic->category->name }}--}}
+							{{--{{ $post->category->name }}--}}
 						{{--</a>--}}
 						
 						<span> • </span>
 						
-						<a href="" title="{{ $topic->user->name }}">
+						<a href="" title="{{ $post->user->name }}">
 							<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-							{{ $topic->user->name }}
+							{{ $post->user->name }}
 						</a>
 						
 						<span> • </span>
 						
 						<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
-						<span class="timeago" title="最后活跃于">{{ $topic->updated_at->diffForHumans() }}</span>
+						<span class="timeago" title="最后活跃于">{{ $post->updated_at->diffForHumans() }}</span>
 					</div>
 				</div>
 			</li>
